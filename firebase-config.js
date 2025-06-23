8d303d4b8f9a53492993e10e0e3ce530455c85bb
tpass-bit <narayanmawale83@gmail.com>
2025-06-23 16:04:20 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBPF1VE82Y3VkZe6IibjqKxBC-XHjM_Wco",
+    authDomain: "chat-2024-ff149.firebaseapp.com",
+    databaseURL: "https://chat-2024-ff149-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-2024-ff149",
+    storageBucket: "chat-2024-ff149.appspot.com",
+    messagingSenderId: "146349109253",
+    appId: "1:146349109253:android:e593afbf0584762519ac6c"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
